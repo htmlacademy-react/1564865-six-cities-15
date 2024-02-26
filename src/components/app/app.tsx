c02ce7aa2fe2ceea4c4d09c60ffa8f2d9226e3ce
Abramof Andrey <abramofandrey2@gmail.com>
2024-02-26 11:21:37 +0300

    Добавит маршрутизацию. для этого в модуле компонента `App` подключит несколько компонентов из пакета `react-router-dom` (`Routes`, `Route`, `BrowserRouter`), а затем обновит код самого компонента: добавит маршрутизацию.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
 import MainPage from '../../pages/main-page/main-page';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
+import { AppRoute } from '../../const';
 
 type NumberOfRentals = {
   offersCount: number;
@@ -6,7 +15,32 @@ type NumberOfRentals = {
 
 function App({offersCount}: NumberOfRentals): JSX.Element {
   return (
-    <MainPage offersCount={offersCount} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={<MainPage offersCount={offersCount} />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={<Favorites />}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<Login />}
+          />
+          <Route
+            path={`${AppRoute.Offer}/:offerId`}
+            element={<Offer />}
+          />
+          <Route
+            path='*'
+            element={<NotFoundPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
