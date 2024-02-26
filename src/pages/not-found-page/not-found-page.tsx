1c438f572a502b769f51af3e947c56a0cd131b0e
Abramof Andrey <abramofandrey2@gmail.com>
2024-02-26 21:20:20 +0300

    добавит роут на странице 404 на главную страницу

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async';
 
 import styles from './not-found-page.module.css';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function NotFoundPage(): JSX.Element {
   return (
@@ -10,7 +12,11 @@ function NotFoundPage(): JSX.Element {
       </Helmet>
       <title>{'6 cities - Not Found'}</title>
       <h1 className={styles.title}>404 Not Found</h1>
-      <p className={styles.text}>Return to the main page
+      <p className={styles.text}>
+        Return to the{' '}
+        <Link to={AppRoute.Root} className={styles.link}>
+          main page
+        </Link>
       </p>
     </div>
   );
