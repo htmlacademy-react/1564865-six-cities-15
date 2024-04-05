fd3f8f3ffc83eaf86876f7e4ec05f742483f6de3
Abramof Andrey <abramofandrey2@gmail.com>
2024-04-05 17:13:26 +0300

    добавит функции сортировки по рейтингу и цене

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { TOffer } from '../types/offer';
+
 export function addPluralEnding(count: number) {
   return count !== 1 ? 's' : '';
 }
@@ -5,3 +7,22 @@ export function addPluralEnding(count: number) {
 export function capitalize(str: string) {
   return str[0].toUpperCase() + str.slice(1);
 }
+
+function sortByRating(offerA: TOffer, offerB: TOffer) {
+  return offerB.rating - offerA.rating;
+}
+
+function sortLowToHigh(offerA: TOffer, offerB: TOffer) {
+  return offerA.price - offerB.price;
+}
+
+function sortHighToLow(offerA: TOffer, offerB: TOffer) {
+  return offerB.price - offerA.price;
+}
+
+export const sorting: Record<string, (offers: TOffer[]) => TOffer[]> = {
+  Popular: (offers: TOffer[]) => offers.slice(),
+  HighToLow: (offers: TOffer[]) => offers.slice().sort(sortHighToLow),
+  LowToHigh: (offers: TOffer[]) => offers.slice().sort(sortLowToHigh),
+  TopRated: (offers: TOffer[]) => offers.slice().sort(sortByRating)
+};
