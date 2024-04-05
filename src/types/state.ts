e9790ab9a73ab5e09875e025030324eb65985401
Abramof Andrey <abramofandrey2@gmail.com>
2024-04-05 19:09:45 +0300

    добавит типы данных AuthData и UserData для представления информации о пользователе. Также добавит перечисление APIRoute для представления доступных маршрутов API.

--- src/types/state.ts
+++ src/types/state.ts
@@ -3,3 +3,14 @@ import { store } from '../store/index';
 export type TState = ReturnType<typeof store.getState>;
 
 export type TAppDispatch = typeof store.dispatch;
+
+export type AuthData = {
+  login: string;
+  password: string;
+};
+
+export type UserData = {
+  id: number;
+  email: string;
+  token: string;
+};
