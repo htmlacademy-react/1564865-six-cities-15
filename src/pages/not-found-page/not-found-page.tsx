733886cff5b21eaa6a99fde3535b068efd5bb89e
Abramof Andrey <abramofandrey2@gmail.com>
2024-04-07 20:12:57 +0300

    изменит компонент NotFoundPage в соответствии с новыми требованиями дизайна

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,23 +1,32 @@
 import { Helmet } from 'react-helmet-async';
-
-import styles from './not-found-page.module.css';
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import Header from '../../components/header/header';
+import Location from '../../components/location/location';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div className={`page ${styles.notFoundPage}`}>
+    <div className="page page--gray page--main">
       <Helmet>
-        <title>{'6 cities - Not Found'}</title>
+        <title>{'6 cities - Not found'}</title>
       </Helmet>
-      <title>{'6 cities - Not Found'}</title>
-      <h1 className={styles.title}>404 Not Found</h1>
-      <p className={styles.text}>
-        Return to the{' '}
-        <Link to={AppRoute.Root} className={styles.link}>
-          main page
-        </Link>
-      </p>
+      <Header />
+      <main className="page__main page__main--index page__main--index-empty">
+        <h1 className="visually-hidden">Cities</h1>
+        <Location />
+        <div className="cities">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">404 Not Found</b>
+                <p className="cities__status-description">
+                  <Link to="/">Вернуться на главную</Link>
+                </p>
+              </div>
+            </section>
+            <div className="cities__right-section"></div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
