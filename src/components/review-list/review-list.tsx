5a5fb1e88d8b8ca41330d98773468551cd44ccb9
Abramof Andrey <abramofandrey2@gmail.com>
2024-04-10 23:28:11 +0300

    добавит сортировку комментариев по дате

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -9,9 +9,11 @@ type TReviewList = {
 
 function ReviewList({ reviews }: TReviewList): JSX.Element {
 
+  const sortedReviews = reviews.slice().sort((reviewA, reviewB) => new Date(reviewB.date).getTime() - new Date(reviewA.date).getTime());
+
   return (
     <ul className="reviews__list">
-      {reviews.map((review) => (
+      {sortedReviews.map((review) => (
         <ReviewsItem key={review.id} review={review} />
       ))}
     </ul>
